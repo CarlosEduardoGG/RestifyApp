729668bfc9b3c0f2775dee665763c34385d3cdc7
CarlosEduardoGG <1430118@upv.edu.mx>
2017-08-16 03:19:21 -0500

    Documentación de la funcion postImagen

--- server.js
+++ server.js
@@ -2,54 +2,59 @@
 var restify = require('restify');
 //Variable para conseguir el get de la base de datos
 var http = require('http');
-//var https = require('https');
 
 //Variables necesarias para poder realizar la conexion a MongoDB
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/restify";
 
-
+/**
+postImagen es la funcion la cual se encarga de la inserción de la imagen
+en la base de datos (codificada en base64), genera la fecha en javascript
+y devuelve una respuesta para confirmar el envio del paquete
+*/
 function postImagen(req,res,next){
   //Código para la conexion a la base de Datos
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
 
     //Código para conseguir la fecha actual
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
+    var today = new Date();//Se crea el objeto de tipo Date
+    var dd = today.getDate();//Se consigue el dia
+    var mm = today.getMonth()+1;//Se consigue el mes y se aumenta en 1 (Enero=1)
+    var yyyy = today.getFullYear();//Se consigue el año actual
+    //Condicion para normalizar el dia
     if(dd<10) {
         dd = '0'+dd
     }
-
+    //Condicion para normalizar el mes
     if(mm<10) {
         mm = '0'+mm
     }
-
+    //Concatenación de la fecha
     today = dd + '/' + mm + '/' + yyyy;
 
-    console.log("body:"+req.body);
-    imagenBD=req.body.imagen+"";
+    console.log("body:"+req.body);//Impresion del cuerpo de la solicitud
+    imagenBD=req.body.imagen+"";//Concatenacion para guardar como texto
 
+    //Se crea el objeto a guardar en MongoDB
     var myobj = { imagen: imagenBD, created:today };
+
+    //Se inserta en la base de datos en la colleccion "imagenes"
     db.collection("imagenes").insertOne(myobj, function(err, res) {
-      if (err){
+      if (err){//En caso de error al insertar
         //res.send(500,"Error al insertar");
         throw err;
       }
-
+      //Notificacion de que un documento se insertó
       console.log("1 document inserted");
+      //Impresion del objeto insertado
       console.log(myobj);
-      //res.send(200,"Archivo insertado");
       db.close();
     });
   });
-
+  //Se envia una respuesta 200 para confirmar OK
   res.send(200);
-
   next();
 }
 
